ebcc1246f0a6cb8edc51461df06e5f7dc584079b
antomuz <antomuz19@gmail.com>
2025-06-09 00:00:00 +0200

    ajout modification et suppresion gamme parmis la,liste (ajout des lien entre front et Activity)

--- app/src/main/java/com/example/robotarmh25_remote/GammeAdapter.java
+++ app/src/main/java/com/example/robotarmh25_remote/GammeAdapter.java
@@ -7,12 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import com.example.robotarmh25_remote.DBHandler;
 import com.example.robotarmh25_remote.EditGammeActivity;
+import com.example.robotarmh25_remote.EditScenarioActivity;
 import com.example.robotarmh25_remote.models.Gamme;
 import com.example.robotarmh25_remote.R;
+import com.example.robotarmh25_remote.models.Scenario;
+
 import java.util.ArrayList;
 
 public class GammeAdapter extends BaseAdapter {
@@ -40,14 +44,38 @@ public class GammeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Gamme gamme = gammes.get(position);
+
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.item_gamme, parent, false);
         }
 
+        Button edit = convertView.findViewById(R.id.btnEdit);
+        Button delete = convertView.findViewById(R.id.btnDelete);
+
         TextView name = convertView.findViewById(R.id.gammeName);
 
-        Gamme gamme = gammes.get(position);
         name.setText(gamme.getName());
+
+        edit.setOnClickListener(v -> {
+            Intent intent = new Intent(context, EditGammeActivity.class);
+            intent.putExtra("id_gamme", gamme.getId_gamme());
+            context.startActivity(intent);
+        });
+
+
+        delete.setOnClickListener(v -> {
+            new AlertDialog.Builder(context)
+                    .setTitle("Supprimer")
+                    .setMessage("Confirmer la suppression de la gamme ?")
+                    .setPositiveButton("Oui", (dialog, which) -> {
+                        dbHandler.deleteGamme(gamme.getId_gamme());
+                        gammes.remove(position);
+                        notifyDataSetChanged();
+                    })
+                    .setNegativeButton("Non", null)
+                    .show();
+        });
 
         return convertView;
     }
